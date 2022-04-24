9763b1c0382b72510c3fc660d30d47c3604b432d
DaffaRomyz <daffaromyzaufa@gmail.com>
2022-04-24 23:06:01 +0700

    tambah kmp

---
+++ src/kmp.go
@@ -0,0 +1,54 @@
+package kmp
+
+/* mengembalikan border function */
+func borderFunction(pattern string) []int {
+
+	var fail []int
+	fail[0] = 0
+	var m int = len(pattern)
+	var i int = 1 // iterator suffiks
+	var j int = 0 // iterator prefiks
+
+	for i < m {
+		if pattern[j] == pattern[i] {
+			fail[i] = j + 1
+			i++
+			j++
+		} else if j > 0 {
+			j = fail[j-1]
+		} else {
+			fail[i] = 0
+			i++
+		}
+	}
+	return fail
+}
+
+/*	menerima text dan pattern,
+	mengembalikan indeks pattern pertama yang ditemukan di text,
+	mengembalikan -1 jika ditak ada pattern dalam text
+*/
+func kmp(text string, pattern string) int {
+	var n int = len(text)
+	var m int = len(pattern)
+
+	var fail []int = borderFunction(pattern)
+
+	var i int = 0 // iterator untuk text
+	var j int = 0 // iterator untuk pattern
+
+	for i < n {
+		if pattern[j] == text[i] {
+			if j == m-1 {
+				return i - m + 1
+			}
+			i++
+			j++
+		} else if j > 0 {
+			j = fail[j-1]
+		} else {
+			i++
+		}
+	}
+	return -1
+}
