94ed322a9e78ab32c16a7e914a905973ffd63100
johannes-ws <johannessukiatmodjo20@gmail.com>
2022-04-29 16:05:56 +0700

    tesDNA.go selesai

--- src/tesDNA.go
+++ src/tesDNA.go
@@ -105,15 +105,14 @@ func bmMatch(text string, pattern string) int {
 }
 
 // main.go
-func checkSequence(filename string) {
+func checkSequence(filename string) bool {
 	content, err := os.ReadFile(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(string(content))
 
 	re := regexp.MustCompile("^[ACTG]+$")
-	fmt.Println(re.Match(content))
+	return re.Match(content)
 }
 
 func readFile(file string) string {
@@ -139,7 +138,6 @@ func main() {
 
 	fmt.Print("Masukkan sequence DNA pengguna: ")
 	fmt.Scanln(&dna_pengguna) // C:\Users\johan\OneDrive\Documents\GitHub\Tubes3_13520063\test\pengguna\
-	checkSequence(dna_pengguna)
 
 	fmt.Print("Masukkan nama penyakit: ")
 	fmt.Scanln(&nama_penyakit) // C:\Users\johan\OneDrive\Documents\GitHub\Tubes3_13520063\test\penyakit\
@@ -149,19 +147,22 @@ func main() {
 	fmt.Print("Masukkan metode: ")
 	fmt.Scanln(&metode)
 
-	if metode == 1 {
-		if bmMatch(readFile(dna_pengguna), readFile(nama_penyakit)) == -1 {
-			hasil += "False"
-		} else {
-			hasil += "True"
+	if checkSequence(dna_pengguna) {
+		if metode == 1 {
+			if bmMatch(readFile(dna_pengguna), readFile(nama_penyakit)) == -1 {
+				hasil += "False"
+			} else {
+				hasil += "True"
+			}
+		} else if metode == 2 {
+			if kmp(readFile(dna_pengguna), readFile(nama_penyakit)) == -1 {
+				hasil += "False"
+			} else {
+				hasil += "True"
+			}
 		}
-	} else if metode == 2 {
-		if kmp(readFile(dna_pengguna), readFile(nama_penyakit)) == -1 {
-			hasil += "False"
-		} else {
-			hasil += "True"
-		}
+		fmt.Printf("Hasil tes: %s - %s - %s - %s", today, nama_pengguna, nama_penyakit, hasil)
+	} else {
+		fmt.Println("Masukan sequence DNA pengguna tidak valid")
 	}
-
-	fmt.Printf("Hasil tes: %s - %s - %s - %s", today, nama_pengguna, nama_penyakit, hasil)
 }
